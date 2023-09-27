195b67e285d56dc65161482cdd0b507e80e96241
Sh_Haydarova <s.haydarzada@gmail.com>
2023-09-27 12:12:21 +0400

    modified

--- src/js/app.js
+++ src/js/app.js
@@ -12,14 +12,14 @@ window.onkeydown = function(event) {
     if (event.keyCode == 32) {
       event.preventDefault();
       
-      if (inputElement) {
-        let currentValue = inputElement.value;
-        inputElement.value = currentValue.slice(0, -1);
+      if (inp) {
+        let currentValue = inp.value;
+        inp.value = currentValue.slice(0, -1);
       }
     }
     
      if (event.key === "Backspace") {
           event.preventDefault();
-          inputElement.value += " ";
+          inp.value += " ";
       }
   }
